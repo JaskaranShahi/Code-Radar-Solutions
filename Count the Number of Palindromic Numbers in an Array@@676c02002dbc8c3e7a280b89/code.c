308a0e81c89557b199fa98a8fb58e21a09a51d8e
Jaskarandeep Singh <jaskaranshahi45@gmail.com>
2025-02-25 20:25:41 +0530

    Solution to Count the Number of Palindromic Numbers in an Array

--- Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
+++ Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
@@ -12,10 +12,10 @@ void main()
     }
     for(int i = 0; i < n; i++)
     {
+        m=0;
+        x=a[i];
         while(a[i]>0)
         {
-            m=0;
-            x=a[i];
             int z=a[i]%10;
             m=m*10+z;
             a[i]=a[i]/10;
