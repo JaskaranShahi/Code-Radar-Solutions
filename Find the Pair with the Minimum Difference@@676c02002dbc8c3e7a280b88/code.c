a9660159ac0ba5e792e50940cda8dbff64fe5625
Jaskarandeep Singh <jaskaranshahi45@gmail.com>
2025-03-31 19:33:46 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main() {
     int n;
     scanf("%d", &n);
@@ -15,21 +27,22 @@ int main() {
         scanf("%d", &a[i]);
     }
 
-    int s = abs(a[0] - a[1]); // Initialize with the first two elements' difference
+    sort(a, n); // Sort the array first
+
+    int s = abs(a[1] - a[0]);
     int x = a[0], y = a[1];
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            int m = abs(a[i] - a[j]);
-            if (m < s) {
-                s = m;
-                x = a[i];
-                y = a[j];
-            }
+    for (int i = 1; i < n - 1; i++) {
+        int m = abs(a[i + 1] - a[i]);
+        if (m < s) {
+            s = m;
+            x = a[i];
+            y = a[i + 1];
         }
     }
 
-    printf("%d %d\n", x < y ? x : y, x > y ? x : y);
+    printf("%d %d\n", x, y);
     return 0;
 }
+
 
